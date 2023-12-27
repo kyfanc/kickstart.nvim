4751a0260ffe161fb751679c7735b25f9ef4c1c3
kyfan <kyfan@email>
2023-12-27 21:16:32 +0800

    add plugin multi line edit

--- init.lua
+++ init.lua
@@ -255,6 +255,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  'mg979/vim-visual-multi',
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
